ad723be0585b150b8b5dfa0d6aa9eca31fcb1652
Noah <noahwinzig@gmail.com>
2022-09-21 15:58:21 -0400

    phase 0 complete

---
+++ backend/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const routes = require('./routes')
+
+
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(cookieParser());
+app.use(express.json());
+
+//security middleware
+
+    //cors
+    if(!isProduction){
+        app.use(cors())
+    }
+
+    //helmt, helps with xss
+    app.use(
+        helmet.crossOriginResourcePolicy({
+            policy: 'cross-origin'
+        })
+    )
+
+    //csurf
+    app.use(
+        csurf({
+            cookie: {
+                secure: isProduction,
+                sameSite: isProduction && 'Lax',
+                httpOnly: true
+            }
+        })
+    )
+
+app.use(routes);
+
+module.exports = app;
