ac1da1e5bf3d10d815262e51b05e7219805f4ce3
Noah <noahwinzig@gmail.com>
2022-09-26 16:21:52 -0400

    adjusted index.js to have correct route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -12,6 +12,7 @@ router.get('/hello/world', function(req, res) {
 
 router.get('/api/csrf/restore', (req,res) => {
     const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken)
     res.status(200).json({
         'XSRF-Token': csrfToken
     })
