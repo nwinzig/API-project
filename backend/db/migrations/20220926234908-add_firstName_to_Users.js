7791a5835153c7f8bdeeef43a4edc6e3140618d4
Noah <noahwinzig@gmail.com>
2022-09-26 19:55:18 -0400

    added migration for first name

---
+++ backend/db/migrations/20220926234908-add_firstName_to_Users.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const { DataTypes } = require("sequelize");
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('Users',
+      'firstName', {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    )
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('Users', 'firstName')
+  }
+};
