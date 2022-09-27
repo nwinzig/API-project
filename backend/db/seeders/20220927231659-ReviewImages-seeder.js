349d5bc39242dadd63051133613f6f97d16f3127
Noah <noahwinzig@gmail.com>
2022-09-27 19:22:20 -0400

    all seeders added and successfully integrated to local database

---
+++ backend/db/seeders/20220927231659-ReviewImages-seeder.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('ReviewImages', [
+      {
+        reviewId: 1,
+        url: 'reviewImage1 url'
+      },
+      {
+        reviewId: 2,
+        url: 'reviewImage2 url'
+      },
+      {
+        reviewId: 3,
+        url: 'reviewImage3 url'
+      },
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('ReviewImages', {
+      reviewId:[1,2,3]
+    })
+  }
+};
