c82bf13b20a58eb90d67f73325caa03e463958df
Noah <noahwinzig@gmail.com>
2022-09-27 13:02:37 -0400

    added seeder data for users and spots

--- backend/db/seeders/20220922213524-demo-user.js
+++ backend/db/seeders/20220922213524-demo-user.js
@@ -6,16 +6,22 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Users', [
       {
+        firstname: 'demo1',
+        lastName: 'user1',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstname: 'demo2',
+        lastName: 'user2',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstname: 'demo3',
+        lastName: 'user3',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
