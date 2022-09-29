15de49fff8d391417cc34cfeaf728fc76f8bca37
Noah <noahwinzig@gmail.com>
2022-09-29 09:51:48 -0400

    get spots of current user

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -179,7 +179,77 @@ router.post('/:spotId/images',requireAuth, async (req,res,next) => {
     )
 })
 
+// get spots for current user
 
+router.get('/current', requireAuth, async (req,res,next) => {
+
+    const userId = req.user.id;
+    console.log(userId)
+
+    const spots = await Spot.findAll(
+        {
+            where: {
+                ownerId: userId
+            },
+            include: [
+                {
+                    model: Review,
+
+                },
+                {
+                    model: SpotImage,
+                }
+            ]
+        }
+    )
+    //finding preview image url
+    let allSpots = [];
+
+    spots.forEach(spot => {
+        allSpots.push(spot.toJSON())
+    });
+
+    allSpots.forEach(spot => {
+        spot.SpotImages.forEach(image => {
+
+            if(image.preview === true){
+            spot.previewImage = image.url
+        }
+            if(image.preview === false){
+                spot.previewImage = "No image url"
+            }
+        })
+        delete spot.SpotImages;
+    })
+
+    //finding average rating
+    allSpots.forEach(spot => {
+        let sum = 0;
+        let count = 0;
+        spot.Reviews.forEach(review => {
+            if(review.stars){
+            sum += review.stars;
+            count++
+            delete spot.Reviews;
+        }
+        })
+        console.log(sum)
+        console.log(count)
+
+        const avg = sum/count;
+
+        console.log(avg)
+
+        if(!isNaN(avg)){
+            spot.avgRating = avg
+        }
+    })
+
+    //return
+
+    res.json(allSpots)
+
+})
 
 
 
