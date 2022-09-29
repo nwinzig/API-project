ac6313d30b76dbcb32a80b61e6f20e5733b59e3a
Noah <noahwinzig@gmail.com>
2022-09-29 19:45:23 -0400

    added bookings routes file

---
+++ backend/routes/api/bookings.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const sequelize = require('sequelize')
+const { Spot } = require('../../db/models');
+const { SpotImage } = require('../../db/models')
+const { Review } = require('../../db/models');
+const {User} = require('../../db/models')
+const router = express.Router();
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
+const { Booking } = require('../../db/models')
+
+
+
+
+
+
+
+module.exports = router;
