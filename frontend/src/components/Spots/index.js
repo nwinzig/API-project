8c16f7ae79bbcb631f90a55e16abc636c21f0d2c
Noah <noahwinzig@gmail.com>
2022-10-17 19:02:10 -0400

    looks like merge fixed itself

--- frontend/src/components/Spots/index.js
+++ frontend/src/components/Spots/index.js
@@ -1,7 +1,7 @@
 
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Route, Switch, NavLink, Link } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import { getSpots } from '../../store/spots';
 
 import './spots.css'
