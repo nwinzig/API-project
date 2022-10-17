94ba2297bec865a90d975bfbb035e841634d7afd
Noah <noahwinzig@gmail.com>
2022-10-17 13:01:49 -0400

    should have resolved merge conflict

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -411,7 +411,7 @@ router.get('/current', requireAuth, async (req,res,next) => {
            // })
 
 
-            const avg = sum/count;
+            // const avg = sum/count;
             if(!isNaN(avg)){
                 spot.avgRating = avg
             }
