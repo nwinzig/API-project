29744e5360d8f05da34c4480590570684945ed5b
Noah <noahwinzig@gmail.com>
2023-01-11 14:11:35 -0500

    added page for no results from search

--- frontend/src/components/SearchResults/index.js
+++ frontend/src/components/SearchResults/index.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 import { useLocation } from "react-router-dom"
+import './search.css'
 
 const SearchResults = () => {
     const location = useLocation()
@@ -11,33 +12,57 @@ const SearchResults = () => {
         console.log('trying to access a prop moved by history push', location.state)
         setSpots(location.state.searchItems)
     },[location])
+    console.log('what is spots if none are found', spots)
 
-    return (
-        <div className='cardHolder'>
-            {spots?.map(spot => (
-                <Link to={`/spots/${spot?.id}`} className='card' key={spot?.id}>
-                    <div className='cardImage'>
-                        <img src={spot?.previewImage} alt='Spot preview'></img>
-                    </div>
-                    <div className='cardInfo'>
-                        <div className='location-rating'>
-                            {/* <div>
+    //creating conditional component to render a different page if there are no results
+    let searchResultsComp;
+    if (spots?.length) {
+        searchResultsComp = (
+            <div className='cardHolder'>
+                {spots?.map(spot => (
+                    <Link to={`/spots/${spot?.id}`} className='card' key={spot?.id}>
+                        <div className='cardImage'>
+                            <img src={spot?.previewImage} alt='Spot preview'></img>
+                        </div>
+                        <div className='cardInfo'>
+                            <div className='location-rating'>
+                                {/* <div>
                                 {spot?.name}
                             </div> */}
-                            <div>
-                                {spot?.city}, {spot?.state}
+                                <div>
+                                    {spot?.city}, {spot?.state}
+                                </div>
+                                <div>
+                                    <i className="fa-solid fa-star"></i>  {spot?.avgRating}
+                                </div>
                             </div>
                             <div>
-                                <i className="fa-solid fa-star"></i>  {spot?.avgRating}
+                                <span className='cardPrice'>${spot?.price}</span> night
                             </div>
                         </div>
-                        <div>
-                            <span className='cardPrice'>${spot?.price}</span> night
-                        </div>
-                    </div>
-                </Link>
-            ))}
-        </div>
+                    </Link>
+                ))}
+            </div>
+        )
+    } else {
+        searchResultsComp =(
+            <div className="noSearchResults">
+                <h3>
+                    We can't find any spots with a similar name. Please try again.
+                </h3>
+                <NavLink
+                to={'/'}
+                className='backHome'
+                >
+                    Back Home
+                </NavLink>
+            </div>
+        )
+    }
+    return (
+        <>
+            {searchResultsComp}
+        </>
     )
 }
 
